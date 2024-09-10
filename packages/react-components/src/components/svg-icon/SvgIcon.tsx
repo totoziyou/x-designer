604456f46e4d0c4eb02b50a96830964e8e586897
WanXing <wanxing@chanjet.com>
2024-09-10 21:32:02 +0800

    wx: components / SvgIcon

---
+++ packages/react-components/src/components/svg-icon/SvgIcon.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {SvgMap} from './svg-map'
+
+export const SvgIcon = (props) => {
+
+    const icon = SvgMap[props.name] || SvgMap.default;
+    const size = props.size || 20;
+
+    let paths;
+    if(icon.type === 'colorsIcon') {
+        paths = icon.paths.map((path, idx) => {
+            return (
+                <path key={idx} d={path.path} fill={path.fill}></path>
+            )
+        })
+    }
+    else {
+        paths = icon.paths.map((path, idx) => {
+            return (
+                <path key={idx} d={path}></path>
+            )
+        })
+    }
+
+    return (
+        <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width={size} height={size}>
+            {paths}
+        </svg>
+    )
+
+}
---
+++ packages/react-components/src/components/svg-icon/index.ts
@@ -0,0 +1,8 @@
+
+import {SvgIcon} from './SvgIcon'
+import {SvgIconRegister} from './svg-map'
+
+export {
+    SvgIcon,
+    SvgIconRegister
+}
