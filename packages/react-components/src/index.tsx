b269d5b34405f9b396128554220a6aca49cb5a0a
WanXing <wanxing@chanjet.com>
2024-09-10 21:33:35 +0800

    wx: components / index export

--- packages/react-components/src/index.tsx
+++ packages/react-components/src/index.tsx
@@ -1,3 +1,5 @@
-/**
- * Created by W.X. on 2024/9/1.
- */
+
+export * from './components/svg-icon'
+export * from './components/props-editor'
+export {SwitchButtons} from './components/switch-buttons/SwitchButtons'
+export {ZoomSize} from './components/zoom-size/ZoomSize'
