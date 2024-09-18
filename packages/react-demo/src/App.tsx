32306e4b5d3c0f2fd214573498fcbfdff57d65c5
WanXing <wanxing@chanjet.com>
2024-09-18 14:44:00 +0800

    wx: demo / add bi demo page

--- packages/react-demo/src/App.tsx
+++ packages/react-demo/src/App.tsx
@@ -5,7 +5,7 @@ import './assets/css/common.less'
 
 const App = () => {
     return (
-        <Framework views={views} default={'BusinessFlow'} />
+        <Framework views={views} default={'BiBoard'} />
     )
 }
 
--- packages/react-demo/src/views/bi-board/BiBoard.tsx
+++ packages/react-demo/src/views/bi-board/BiBoard.tsx
@@ -1,11 +1,22 @@
 import React, {useRef} from 'react'
+import BI from '@x-designer/react-bi'
 import './BiBoard.less'
 
 export const BiBoard = () => {
 
     return (
-        <div className="x-framework-page x-bi-board">
-            BiBoard!
+        <div className="x-framework-page xbi-board">
+            <div className="xb-flow-header">
+                BI数据可视化
+            </div>
+            <div className="xb-flow-body">
+                <div className="xb-flow-list">
+                    <div className="xb-flow-list-header">Board列表</div>
+                </div>
+                <div className="xb-flow-designer">
+                    <BI.BiDesigner />
+                </div>
+            </div>
         </div>
     )
 
